34af45c03526eb059e25cac369e0fcd430bc29f3
Fabio Colacio <fabio.colacio.dev@gmail.com>
2018-09-08 21:22:26 -0700

    use IO::Socket

--- server.pl
+++ server.pl
@@ -1,24 +1,36 @@
 #!/bin/perl -w
 
 use strict;
+use IO::Socket;
 use Socket;
 
+my $host = "0.0.0.0";
 my $port = 8080;
-my $proto = getprotobyname "tcp";
+my $proto = "tcp";
+my $backlog = 5;
 
-socket(SERVER, PF_INET, SOCK_STREAM, $proto)
-    or die "socket: $!";
+my $server = new IO::Socket::INET(
+    LocalHost => $host,
+    LocalPort => $port,
+    Proto     => $proto,
+    Listen    => $backlog
+) or die "server: $!";
 
-setsockopt(SERVER, SOL_SOCKET, SO_REUSEADDR, pack("l", 1))
-    or die "setsockopt: $!";
 
-bind(SERVER, sockaddr_in($port, INADDR_ANY))
-    or die "bind: $!";
-
-listen(SERVER, SOMAXCONN);
+# socket(SERVER, PF_INET, SOCK_STREAM, $proto)
+#     or die "socket: $!";
+# 
+# setsockopt(SERVER, SOL_SOCKET, SO_REUSEADDR, pack("l", 1))
+#     or die "setsockopt: $!";
+# 
+# bind(SERVER, sockaddr_in($port, INADDR_ANY))
+#     or die "bind: $!";
+# 
+# listen(SERVER, SOMAXCONN);
 
 for (;;) {
-    my $paddr = accept(CLIENT, SERVER);
+    # my $paddr = accept(CLIENT, SERVER);
+    my $client = $server->accept;
 
     my $message = "";
     my $message_size = 0;
@@ -26,12 +38,12 @@ for (;;) {
     my $eof = 0;
 
     while ($message !~ m/\r\n/) {
-        $message_size += read(CLIENT, $message, $chunk_size, $message_size);
+        $message_size += read($client, $message, $chunk_size, $message_size);
     }
 
     print $message;
 
-    print(CLIENT
+    print($client
         "HTTP/1.1 200 OK\r\n\r\n
         <!doctype html>
         <html>
@@ -43,6 +55,8 @@ for (;;) {
             </body>
         </html>");
 
-    close(CLIENT);
+    close($client);
 }
+
+$server->close;
 

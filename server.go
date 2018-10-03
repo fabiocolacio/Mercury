a38193ab3978294819beae73bab99a443f6cce6a
Fabio Colacio <fabio.colacio.dev@gmail.com>
2018-10-03 12:09:57 -0700

    Use 256 bit AES key

--- server.go
+++ server.go
@@ -44,8 +44,8 @@ func NewServerWithConf(conf Config) (*Server, error) {
         MinVersion: tls.VersionTLS12,
         PreferServerCipherSuites: true,
         CipherSuites: []uint16{
-            tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-            tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+            tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+            tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
         },
     }
 

59ab4cc29f50ce7f43b52d397a2c17ca05cbd394
Fabio Colacio <fabio.colacio.dev@gmail.com>
2018-11-18 17:12:54 -0800

    Improve install script

--- install.sh
+++ install.sh
@@ -1,94 +1,73 @@
 #!/bin/bash
 
-if [ "$EUID" -ne 0 ]; then
-    echo "Please run this script as root!"
-    exit
+# This script installs Mercury with it's default
+# configuration. The default configuration doesn't
+# require admin priveleges to install or run, as it
+# will install to ~/.go/bin, and bind to ports 8080
+# and 9090.
+
+#== DEFAULT CONFIGURATION ==#
+CONF_DIR=$HOME/.config/mercury
+CONF_FILE=$CONF_DIR/config.toml
+LOG_FILE=$CONF_DIR/log.txt
+KEY_FILE=$CONF_DIR/privkey.pem
+CERT_FILE=$CONF_DIR/cert.pem
+CERT_EXP=365
+SQL_USER="root"
+SQL_PASS=""
+SQL_DB="mercury"
+HTTP_ADDR=":8080"
+HTTPS_ADDR=":9090"
+
+#== INSTALL BINARY ==#
+if [ -z "$(command -v go)" ]; then
+    echo "go tools are not installed!"
+    echo "Please install golang at https://golang.org/dl and try again."
+    exit 1
 fi
 
-if ! [ -x "$(command -v go)" ]; then
-    echo "You must install golang to compile this program!"
-    echo "You can get it at https://golang.org/dl/"
-    exit
+if [ -z "$GOPATH" ]; then
+    echo 'export GOPATH=$HOME/.go'
+    echo 'export PATH=$PATH:$GOPATH/bin' >> ~/.bashrc
+    source ~/.bashrc
 fi
 
-BIN_DIR=/usr/local/bin
-DATA_DIR=/usr/local/share/com.github.fabiocolacio.mercury-server
-CONF_FILE=$DATA_DIR/config.toml
+echo "Installing mercury to $GOPATH/bin"
+go get github.com/fabiocolacio/mercury
 
-go build
-mv mercury-server $BIN_DIR
-mkdir $DATA_DIR
-cp res/sample-config.toml $DATA_DIR
-
-echo ""
-echo "You can install mercury-server as a service."
-echo "Doing so will launch the server on system-startup, and keep it alive if it fails."
-echo "These systems also provide useful command-line tools to manage the service."
-echo "Would you like to install mercury-server as a service?"
-echo "1) Create a launchd service. (macOS)"
-echo "2) Create a systemd service. (linux)"
-echo "3) don't create a service"
-read service
-echo ""
-
-case $service in
-    "1")
-        cp res/com.github.fabiocolacio.mercury-server.plist /Library/LaunchDaemons
-        echo "launchd service created"
-        echo "To start the service manually, use the following command:"
-        echo "'sudo launchctl load /Library/LaunchDaemons/com.github.fabiocolacio.mercury-server.plist'\n"
-        ;;
-    "2")
-        cp res/mercury-server.service /lib/systemd/system
-        echo "systemd service created"
-        echo "To start the service manually, use the following command:"
-        echo "'sudo systemctl start mercury-server'\n"
-        ;;
-    *)
-        echo "No service files were installed."
-        echo "Use the command 'mercury-server' to start the server.\n"
-        ;;
-esac
-
-echo "Before you can run the server, you need to create a configuration file for it."
-echo "Do you want to interactively create one now? (y/n)"
-read createconfig
-echo ""
-
-if [ "$createconfig" = "y" ]
-then
-    echo "What address and port should Mercury listen to HTTP connections on?"
-    echo "To bind to all interfaces (recommended), use the address '0.0.0.0'"
-    echo "Enter it in the format 'address:port'"
-    read httpaddr
-    echo "HttpAddr = \"$httpaddr\"" > $CONF_FILE
-    echo ""
-
-    echo "What address and port should Mercury listen to HTTPS connections on?"
-    echo "To bind to all interfaces (recommended), use the address '0.0.0.0'"
-    echo "Enter it in the format 'address:port'"
-    read httpsaddr
-    echo "HttpsAddr = \"$httpsaddr\"" >> $CONF_FILE
-    echo ""
-
-    echo "What is the absolute path to your SSL Certificate?"
-    read certfile
-    echo "CertFile = \"$certfile\"" >> $CONF_FILE
-    echo ""
-
-    echo "What is the absolute path to your Private Key?"
-    read keyfile
-    echo "KeyFile = \"$keyfile\"" >> $CONF_FILE
-    echo ""
-
-    echo "Configuration file written to '$CONF_FILE'"
-    echo ""
-else
-    echo "Please place a config in the following location:"
-    echo "$DATA_DIR\n"
-
-    echo "Alternatively, you can pass a config file as a cli argument"
-    echo ""
+#== INSTALL CONFIG FILES ==#
+if [ -z "$(command -v openssl)" ]; then
+    echo "openssl tools are not installed!"
+    echo "Please install openssl and try again."
+    exit 1
 fi
 
-echo "Installation Complete!"
+mkdir -p $CONF_DIR
+
+if [ ! -e $KEY_FILE ] || [ ! -e $CERT_FILE ]; then
+    echo "Creating private key as $KEY_FILE"
+    openssl genrsa -out $KEY_FILE 2048
+
+    echo "Creating self-signed certificate as $CERT_FILE"
+    openssl req -new -x509 -sha256 -key $KEY_FILE -out $CERT_FILE -days $CERT_EXP
+fi
+
+if [ ! -e $LOG_FILE ]; then
+    echo "Creating log file as $LOG_FILE"
+    touch $LOG_FILE
+fi
+
+if [ ! -e $CONF_FILE ]; then
+    echo "Installing config as $CONF_FILE"
+    echo "HttpAddr = \"$HTTP_ADDR\"" >> $CONF_FILE
+    echo "HttpsAddr = \"$HTTPS_ADDR\"" >> $CONF_FILE
+    echo "CertFile = \"$CERT_FILE\"" >> $CONF_FILE
+    echo "KeyFile = \"$KEY_FILE\"" >> $CONF_FILE
+    echo "LogFile = \"$LOG_FILE\"" >> $CONF_FILE
+    echo "SQLUser = \"$SQL_USER\"" >> $CONF_FILE
+    echo "SQLPass = \"$SQL_PASS\"" >> $CONF_FILE
+    echo "SQLDb = \"$SQL_DB\"" >> $CONF_FILE
+fi
+
+echo "Installation complete."
+
